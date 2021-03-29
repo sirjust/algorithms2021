627dabd43214d42f41f104506083096119e1a42d
Justin Moore <theoperaking@yahoo.com>
2021-03-29 18:35:58 -0400

    Remove linked list duplicates

---
+++ AlgorithmTests/LinkedListAlorithmsTests.cs
@@ -0,0 +1,72 @@
+﻿using Algorithms2021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class LinkedListAlorithmsTests
+    {
+        [TestMethod]
+        public void RemoveDuplicatesSucceeds_WhenOneDuplicate()
+        {
+            // Arrange
+            IntegerLinkedList list = new IntegerLinkedList { Head = new IntegerNode(5) };
+            list.AddToFront(5);
+            list.AddToFront(3);
+
+            // Act
+            list.RemoveDuplicates();
+
+            // Assert
+            Assert.AreEqual(list.Head.Data, 3);
+            Assert.AreEqual(list.Head.Next.Data, 5);
+            Assert.IsNull(list.Head.Next.Next);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesSucceeds_WhenMultipleDuplicate()
+        {
+            // Arrange
+            IntegerLinkedList list = new IntegerLinkedList { Head = new IntegerNode(5) };
+            list.AddToFront(12);
+            list.AddToFront(5);
+            list.AddToFront(12);
+            list.AddToFront(5);
+            list.AddToFront(3);
+            list.AddToFront(-6);
+
+            // Act
+            list.RemoveDuplicates();
+
+            // Assert
+            Assert.AreEqual(-6, list.Head.Data);
+            Assert.AreEqual(3, list.Head.Next.Data);
+            Assert.AreEqual(5, list.Head.Next.Next.Data);
+            Assert.AreEqual(12, list.Head.Next.Next.Next.Data);
+            Assert.IsNull(list.Head.Next.Next.Next.Next);
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesSucceeds_WhenNoDuplicates()
+        {
+            // Arrange
+            IntegerLinkedList list = new IntegerLinkedList { Head = new IntegerNode(4) };
+            list.AddToFront(3);
+            list.AddToFront(2);
+            list.AddToFront(1);
+
+            // Act
+            list.RemoveDuplicates();
+
+            // Assert
+            Assert.AreEqual(1, list.Head.Data);
+            Assert.AreEqual(2, list.Head.Next.Data);
+            Assert.AreEqual(3, list.Head.Next.Next.Data);
+            Assert.AreEqual(4, list.Head.Next.Next.Next.Data);
+            Assert.IsNull(list.Head.Next.Next.Next.Next);
+        }
+    }
+}
---
+++ Algorithms2021/LinkedListAlgorithms.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms2021
+{
+    public static class LinkedListAlgorithms
+    {
+        
+    }
+
+    public class IntegerNode
+    {
+        public int Data { get; set; }
+        public IntegerNode Next { get; set; }
+
+        public IntegerNode(int data)
+        {
+            Data = data;
+        }
+
+        public void SetNext(IntegerNode next)
+        {
+            Next = next;
+        }
+    }
+
+    public class IntegerLinkedList
+    {
+        public IntegerNode Head { get; set; }
+        public void AddToFront(int value)
+        {
+            IntegerNode newNode = new IntegerNode(value);
+
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public void RemoveDuplicates()
+        {
+            if (Head == null || Head.Next == null) return;
+
+            var outsideCurrent = Head;
+            {
+                while(outsideCurrent != null)
+                {
+                    var current = outsideCurrent.Next;
+                    var previous = outsideCurrent;
+                    var value = outsideCurrent.Data;
+                    while (current != null)
+                    {
+                        if (current.Data == value)
+                        {
+                            previous.Next = current.Next;
+                        }
+
+                        previous = current;
+                        current = current.Next;
+                    }
+                    outsideCurrent = outsideCurrent.Next;
+                }
+            }
+        }
+    }
+}
