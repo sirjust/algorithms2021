3e80542204b5e244ed355ebc50b80ab0f1a7ef03
Justin Moore <theoperaking@yahoo.com>
2021-04-05 20:48:13 -0400

    Add merge sort algorithm

---
+++ AlgorithmTests/SortersTests.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms2021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class SortersTests
+    {
+        [TestMethod]
+        public void MergeSort_ShouldSucceed()
+        {
+            // Arrange
+            var testArray = new[] { 50, 20, -1, 5, -10, 31 };
+            var expected = new[] { -10, -1, 5, 20, 31, 50 };
+
+            // Act
+            Sorters.MergeSort(testArray, 0, testArray.Length);
+
+            // Assert
+            Assert.IsTrue(testArray.SequenceEqual(expected));
+        }
+    }
+}
---
+++ Algorithms2021/Sorters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms2021
+{
+    public static class Sorters
+    {
+        public static void MergeSort(int[] array, int start, int end)
+        {
+            if (end - start < 2) return;
+
+            // partition the sections
+            int mid = (start + end) / 2;
+            MergeSort(array, start, mid);
+            MergeSort(array, mid, end);
+
+            // merge them
+            Merge(array, start, mid, end);
+        }
+
+        public static void Merge(int[] array, int start, int mid, int end)
+        {
+            if (array[mid - 1] <= array[mid]) return;
+
+            int i = start;
+            int j = mid;
+            int tempIndex = 0;
+
+            int[] temp = new int[end - start];
+            while (i < mid && j < end)
+            {
+                // the tempIndex is incremented, and whichever value it takes
+                temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+
+            }
+            Array.Copy(array, i, array, start + tempIndex, mid - i);
+            Array.Copy(temp, 0, array, start, tempIndex);
+        }
+    }
+}
